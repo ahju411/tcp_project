b8cc53439239ee20e2cef475d63c2086ee94a72e
Yoo <isz06188@naver.com>
2021-11-14 14:58:02 +0900

    d

---
+++ pyqt5연습/yootest.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+from_class = uic.loadUiType("tcp_project\\pyqt5연습\\MainWindow - untitled.ui")[0]
+
+class WindowClass(QMainWindow, from_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+
+app = QApplication(sys.argv)
+mainWindow = WindowClass()
+mainWindow.show()
+app.exec_()
