5f5bbaeba0a7e017725ee45946be01fe2b25d682
Yoo <isz06188@naver.com>
2021-11-15 22:58:32 +0900

    ㅁㅁ

--- mafia/yoo_gamerule.py
+++ mafia/yoo_gamerule.py
@@ -1,15 +1,30 @@
+import random
 
-mafia_su=1
-citizen_su=1
+## 게임 들어오면 직업을 부여, 8명될경우 게임 시작
+
+player = ["","","","","","","",""]
+count = 0
+for i in player:
+    num = random.sample(range(0,1))
+    player[0] = num
+for j in player:
+    if player[j] == 1 :
+        count = count + 1
+        if count == 2 :
+            player[j] = ""
+            i = i - 1
 
 
-def ifWin(mafia_su,citizen_su):
-    if mafia_su==citizen_su :
-        print("마피아팀 승리 입니다.")
-
-    elif mafia_su==0 :
-        print("시민팀의 승리 입니다.")
+print(player[0] , player[1] , player[2]
+, player[3], player[4], player[5], player[6], player[7], player[8])
+            
+    
 
 
+def Ifwin(Mafia_su,Citizen_su): # 마피아, 시민팀 승리조건
+    
+    if Mafia_su==Citizen_su :
+        print("Mafia Team Win ")
 
-ifWin(mafia_su,citizen_su)
+    elif Mafia_su==0 :
+        print("Citizen Team Win")
