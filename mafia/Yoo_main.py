ed4f47c6e687a3c6aa4848f25acfb889f548c5fe
Yoo <isz06188@naver.com>
2021-11-16 23:02:57 +0900

    aaaaaaaa

---
+++ mafia/Yoo_main.py
@@ -0,0 +1,15 @@
+
+
+import random
+import threading
+import yoo_Time
+
+
+mafia_life = 2
+citizen_life = 4
+Date = 0
+count = 0
+while(mafia_life==0 or mafia_life == citizen_life):
+    yoo_Time.start_timer()
+
+ 
