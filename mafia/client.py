c69a5673ffb6c72e4935985622a1729edd3f84c7
ahju411 <hshs321@naver.com>
2021-11-16 19:08:57 +0900

    멀티쓰레드

---
+++ mafia/client.py
@@ -0,0 +1,25 @@
+import socket
+import threading
+
+def Send(client_sock):
+    while True:
+        send_data = bytes(input('내 닉네임: ').encode())
+        client_sock.send(send_data)
+
+def Recv(client_sock):
+    while True:
+        recv_data = client_sock.recv(1024).decode()
+        print(recv_data)
+
+if __name__=='__main__':
+    client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    Host = '127.0.0.1'
+    Port = 9000
+    client_sock.connect(('127.0.0.1',9000))
+    print('Connecting to',Host,Port)
+
+    thread1 = threading.Thread(target=Send,args=(client_sock,))
+    thread1.start()
+
+    thread2 = threading.Thread(target=Recv, args=(client_sock,))
+    thread2.start()
