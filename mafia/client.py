61cf46fbf3118873d7c37c96e2fd5451a46ea9f4
ahju411 <hshs321@naver.com>
2021-11-16 22:58:58 +0900

    서버구현

--- mafia/client.py
+++ mafia/client.py
@@ -2,24 +2,43 @@ import socket
 import threading
 
 def Send(client_sock):
+    #global nickname 
+    #nickname= input('닉네임을 입력하세요: ')
+    #client_sock.send(nickname.encode('utf-8'))
     while True:
-        send_data = bytes(input('내 닉네임: ').encode())
-        client_sock.send(send_data)
+        send_data = input()
+        client_sock.send(send_data.encode('utf-8'))
+       # print(send_data)
+        if send_data == '/q':
+            print('연결 종료')
+            client_sock.close()
 
-def Recv(client_sock):
-    while True:
-        recv_data = client_sock.recv(1024).decode()
-        print(recv_data)
+def Recv(client_sock,user):
+    while 1:
+        try:
+            recv_data = client_sock.recv(1024).decode()
+            
+        except:
+            print('연결끊김')
+            break
+       
+        if not user in recv_data:
+           # print('실행되냐')
+            print(recv_data)
 
 if __name__=='__main__':
     client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    Host = '127.0.0.1'
-    Port = 9000
-    client_sock.connect(('127.0.0.1',9000))
+    Host = '192.168.35.245'
+    Port = 9090
+    user = input('닉네임: ')
+    client_sock.connect((Host,Port))
+    client_sock.send(user.encode('utf-8'))
     print('Connecting to',Host,Port)
 
-    thread1 = threading.Thread(target=Send,args=(client_sock,))
-    thread1.start()
-
-    thread2 = threading.Thread(target=Recv, args=(client_sock,))
+    thread2 = threading.Thread(target=Recv, args=(client_sock,user))
+   # thread2.daemon = True
     thread2.start()
+
+    thread1 = threading.Thread(target=Send,args=(client_sock,))
+   # thread1.daemon=True
+    thread1.start()
--- mafia/server.py
+++ mafia/server.py
@@ -1,67 +1,60 @@
-import socket
-import threading
-from queue import Queue
+import socket 
+import argparse 
+import threading 
+import time 
 
+host = "0.0.0.0" 
+port = 9090 
+user_list = {} 
+notice_flag = 0 
 
-def Send(group, send_queue):
-    print('Thread Send Start')
-    while True:
-        try:
-            #새롭게 추가된 클라이언트가 있을 경우 Send 쓰레드를 새롭게 만들기 위해 루프를 빠져나감
-            recv = send_queue.get()
-            if recv == 'Group Changed':
-                print('Group Changed')
-                break
+def msg_func(msg): 
+    print(msg) 
+    for con in user_list.values(): 
+        try: 
+            con.send(msg.encode('utf-8')) 
+        except: 
+            print("연결이 비 정상적으로 종료된 소켓 발견") 
 
+def handle_receive(client_socket, addr, user): 
+    msg = "---- %s님이 들어오셨습니다. ----"%user 
+    msg_func(msg) 
+    while 1: 
+        data = client_socket.recv(1024) 
+        string = data.decode('utf-8') 
+        if "/q" in string:
+            msg = "---- %s님이 나가셨습니다. ----"%user #유저 목록에서 방금 종료한 유저의 정보를 삭제 
+            del user_list[user] 
+            msg_func(msg) 
+            break 
+        string = "%s : %s"%(user, string) 
+        msg_func(string) 
+    client_socket.close() 
 
-            #for 문을 돌면서 모든 클라이언트에게 동일한 메시지를 보냄
-            for conn in group:
-                msg = 'Client' + str(recv[2]) + ' >> ' + str(recv[0])
-                if recv[1] != conn: #client 본인이 보낸 메시지는 받을 필요가 없기 때문에 제외시킴
-                    conn.send(bytes(msg.encode()))
-                else:
-                    pass
-        except:
-            pass
+def handle_notice(client_socket, addr, user):
+     pass 
 
-
-def Recv(conn, count, send_queue):
-    print('Thread Recv' + str(count) + ' Start')
-    while True:
-        data = conn.recv(1024).decode()
-        send_queue.put([data, conn, count]) #각각의 클라이언트의 메시지, 소켓정보, 쓰레드 번호를 send로 보냄
-
-
-# TCP Echo Server
-if __name__ == '__main__':
-    send_queue = Queue()
-    HOST = ''  # 수신 받을 모든 IP를 의미
-    PORT = 9000  # 수신받을 Port
-    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP Socket
-    server_sock.bind((HOST, PORT))  # 소켓에 수신받을 IP주소와 PORT를 설정
-    server_sock.listen(10)  # 소켓 연결, 여기서 파라미터는 접속수를 의미
-    count = 0
-    group = [] #연결된 클라이언트의 소켓정보를 리스트로 묶기 위함
-    while True:
-        count = count + 1
-        conn, addr = server_sock.accept()  # 해당 소켓을 열고 대기
-        group.append(conn) #연결된 클라이언트의 소켓정보
-        print('Connected ' + str(addr))
-
-
-        #소켓에 연결된 모든 클라이언트에게 동일한 메시지를 보내기 위한 쓰레드(브로드캐스트)
-        #연결된 클라이언트가 1명 이상일 경우 변경된 group 리스트로 반영
-
-        if count > 1:
-            send_queue.put('Group Changed')
-            thread1 = threading.Thread(target=Send, args=(group, send_queue,))
-            thread1.start()
-            pass
-        else:
-            thread1 = threading.Thread(target=Send, args=(group, send_queue,))
-            thread1.start()
-
-        #소켓에 연결된 각각의 클라이언트의 메시지를 받을 쓰레드
-        thread2 = threading.Thread(target=Recv, args=(conn, count, send_queue,))
-        thread2.start()
-
+def accept_func(): #IPv4 체계, TCP 타입 소켓 객체를 생성 
+     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #포트를 사용 중 일때 에러를 해결하기 위한 구문 
+     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #ip주소와 port번호를 함께 socket에 바인드 한다.#포트의 범위는 1-65535 사이의 숫자를 사용할 수 있다. 
+     server_socket.bind((host,9090)) #서버가 최대 5개의 클라이언트의 접속을 허용한다. 
+     server_socket.listen(10) 
+     while 1: 
+        try: #클라이언트 함수가 접속하면 새로운 소켓을 반환한다. 
+             client_socket, addr = server_socket.accept() 
+        except KeyboardInterrupt: 
+             for user, con in user_list: 
+                 con.close() 
+                 server_socket.close() 
+                 print("Keyboard interrupt") 
+                 break 
+        user = client_socket.recv(1024).decode('utf-8') 
+        user_list[user] = client_socket #accept()함수로 입력만 받아주고 이후 알고리즘은 핸들러에게 맡긴다. 
+        notice_thread = threading.Thread(target=handle_notice, args=(client_socket, addr, user)) 
+        notice_thread.daemon = True 
+        notice_thread.start() 
+        receive_thread = threading.Thread(target=handle_receive, args=(client_socket, addr,user)) 
+        receive_thread.daemon = True 
+        receive_thread.start() 
+if __name__ == '__main__':     
+    accept_func()
