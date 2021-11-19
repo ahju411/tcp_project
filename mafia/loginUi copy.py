057bcabd6aa8ea64400f50b592fa40003eb8ea30
Yoo <isz06188@naver.com>
2021-11-19 23:46:41 +0900

    aa

---
+++ mafia/loginUi copy.py
@@ -0,0 +1,204 @@
+#로그인창
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys,res,login
+from registerUi import Ui_registerUi
+from chkuser import Ui_chkuser
+
+class Ui_Form(QtWidgets.QWidget):
+    def __init__(self):
+            super(Ui_Form,self).__init__()
+            self.openWindow()
+            self.show()
+    def openWindow(self):
+        self.setupUi(self)
+        self.pushButton_2.clicked.connect(self.register) # type: ignore
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(450, 550)
+        Form.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        Form.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.widget = QtWidgets.QWidget(Form)
+        self.widget.setGeometry(QtCore.QRect(30, 30, 370, 480))
+        self.widget.setStyleSheet("QPushButton#pushButton{\n"
+"    background-color: qlineargradient(spread:pad, x1:0, y1:0.505682, x2:1, y2:0, stop:0 rgba(255, 255, 255, 0.25), stop:1 rgba(255, 255, 255, 0.4));\n"
+"    color:rgba(255,255,255,255);\n"
+"    border-radius:5px;\n"
+"}\n"
+"QPushButton#pushButton:hover{\n"
+"    background-color: qlineargradient(spread:pad, x1:0, y1:0.505682, x2:1, y2:0, stop:0 rgba(255, 255, 255, 0.3), stop:1 rgba(255, 255, 255, 0.4));\n"
+"}\n"
+"QPushButton#pushButton:pressed{\n"
+"    padding-left:5px;\n"
+"    padding-top:5px;\n"
+"    background-color:rgba(105,118,132,200);\n"
+"}\n"
+"QPushButton#pushButton_2{\n"
+"    background-color: qlineargradient(spread:pad, x1:0, y1:0.505682, x2:1, y2:0, stop:0 rgba(255, 255, 255, 0.25), stop:1 rgba(255, 255, 255, 0.4));\n"
+"    color:rgba(255,255,255,255);\n"
+"    border-radius:5px;\n"
+"}\n"
+"QPushButton#pushButton_2:hover{\n"
+"    background-color: qlineargradient(spread:pad, x1:0, y1:0.505682, x2:1, y2:0, stop:0 rgba(255, 255, 255, 0.3), stop:1 rgba(255, 255, 255, 0.4));\n"
+"}\n"
+"QPushButton#pushButton_2:pressed{\n"
+"    padding-left:5px;\n"
+"    padding-top:5px;\n"
+"    background-color:rgba(105,118,132,200);\n"
+"}\n"
+"QPushButton#pushButton_3{\n"
+"    color:rgba(255,255,255,140);\n"
+"    border: 0px;\n"
+"}")
+        self.widget.setObjectName("widget")
+        self.label = QtWidgets.QLabel(self.widget)
+        self.label.setGeometry(QtCore.QRect(40, 10, 300, 420))
+        self.label.setStyleSheet("border-image: url(:/images/images/login.jpg);\n"
+"border-radius:20px")
+        self.label.setText("")
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.widget)
+        self.label_2.setGeometry(QtCore.QRect(40, 10, 301, 420))
+        self.label_2.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:0, stop:0 rgba(0, 0, 0, 9), stop:0.375 rgba(0, 0, 0, 50),stop:0.835226 rgba(0,0,0,75));\n"
+"border-radius: 15px")
+        self.label_2.setText("")
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.widget)
+        self.label_3.setGeometry(QtCore.QRect(50, 20, 280, 390))
+        self.label_3.setStyleSheet("background-color:rgba(0,0,0,100);\n"
+"border-radius:15px;")
+        self.label_3.setText("")
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.widget)
+        self.label_4.setGeometry(QtCore.QRect(155, 95, 111, 40))
+        font = QtGui.QFont()
+        font.setFamily("Harrington")
+        font.setPointSize(20)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_4.setFont(font)
+        self.label_4.setStyleSheet("color:rgba(255,255,255,210);")
+        self.label_4.setObjectName("label_4")
+        self.lineEdit = QtWidgets.QLineEdit(self.widget)
+        self.lineEdit.setGeometry(QtCore.QRect(80, 165, 200, 40))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.lineEdit.setFont(font)
+        self.lineEdit.setStyleSheet("background-color:rgba(0,0,0,0);\n"
+"border:none;\n"
+"border-bottom:2px solid rgba(105,118,132,255);\n"
+"color:rgba(255,255,255,230);\n"
+"padding-bottom:7px")
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.widget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(80, 230, 200, 40))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.lineEdit_2.setFont(font)
+        self.lineEdit_2.setStyleSheet("background-color:rgba(0,0,0,0);\n"
+"border:none;\n"
+"border-bottom:2px solid rgba(105,118,132,255);\n"
+"color:rgba(255,255,255,230);\n"
+"padding-bottom:7px")
+        self.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.pushButton = QtWidgets.QPushButton(self.widget)
+        self.pushButton.setGeometry(QtCore.QRect(80, 290, 200, 40))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.widget)
+        self.pushButton_2.setGeometry(QtCore.QRect(80, 340, 200, 40))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setStyleSheet("")
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(self.widget)
+        self.pushButton_3.setGeometry(QtCore.QRect(140, 390, 91, 23))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.label_2.raise_()
+        self.label.raise_()
+        self.label_3.raise_()
+        self.label_4.raise_()
+        self.lineEdit.raise_()
+        self.lineEdit_2.raise_()
+        self.pushButton.raise_()
+        self.pushButton_2.raise_()
+        self.pushButton_3.raise_()
+        
+        
+        self.label.setGraphicsEffect(QtWidgets.QGraphicsDropShadowEffect(blurRadius=25, xOffset=0, yOffset=0, color=QtGui.QColor(234, 221, 186, 100)))
+        self.label_3.setGraphicsEffect(QtWidgets.QGraphicsDropShadowEffect(blurRadius=25, xOffset=0, yOffset=0, color=QtGui.QColor(105, 118, 132, 100)))
+        self.pushButton.setGraphicsEffect(QtWidgets.QGraphicsDropShadowEffect(blurRadius=25, xOffset=3, yOffset=3, color=QtGui.QColor(105, 118, 132, 100)))
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+        self.lineEdit_2.returnPressed.connect(self.login)
+        self.pushButton.clicked.connect(self.login) # type: ignore
+        self.pushButton_2.clicked.connect(self.register) # type: ignore
+        self.pushButton_3.clicked.connect(self.resetpw)
+       
+    def login(self): #로그인 창에서 로그인 실행 
+            id = self.lineEdit.text()
+            pw = self.lineEdit_2.text()
+            value = (id,pw)
+            myname,myid = login.login(value)
+            if myname == 1:
+                    QtWidgets.QMessageBox.about(self,"알림","아이디 또는 비밀번호가 틀립니다.")
+                    self.lineEdit.clear()
+                    self.lineEdit_2.clear()
+            else:
+                    #myname,myid = login.login(value)
+                    QtWidgets.QMessageBox.about(self,"알림","로그인 성공.")
+                    print(myname,myid)
+
+    def register(self): #가입하기 버튼 누르면 실행됨 
+        joinui = Ui_registerUi()
+        joinui.backbtn.clicked.connect(self.tologinform)
+        
+        widgetlog.addWidget(joinui)
+        widgetlog.setCurrentIndex(widgetlog.currentIndex()+1) #가입창이 메인 위젯
+        
+        #self.join.close()
+        #self.join.show()
+    def tologinform(self):
+        #다시 로그인 창으로 돌아감 
+        widgetlog.setCurrentIndex(widgetlog.currentIndex()-2)
+    def resetpw(self):
+        #어떤 ui가 떠야지 본인 확인 ui 그리고 비번 재설정 ui 
+        self.this = Ui_Form()
+        self.chk = Ui_chkuser()
+        self.chk.show()
+        self.this.close()
+        #self.this.show()
+    def pushback(self):
+        widgetlog.setCurrentIndex(widgetlog.currentIndex())
+
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.label_4.setText(_translate("Form", "Mafia"))
+        self.lineEdit.setPlaceholderText(_translate("Form", "User Name"))
+        self.lineEdit_2.setPlaceholderText(_translate("Form", "Password"))
+        self.pushButton.setText(_translate("Form", "Log In"))
+        self.pushButton_2.setText(_translate("Form", "Join In"))
+        self.pushButton_3.setText(_translate("Form", "비밀 번호 찾기"))
+if __name__=='__main__':
+        QtWidgets.QApplication.processEvents()
+        app = QtWidgets.QApplication(sys.argv)
+        logui = Ui_Form()
+        widgetlog = QtWidgets.QStackedWidget()
+        widgetlog.addWidget(logui)
+        widgetlog.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        widgetlog.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        widgetlog.setFixedHeight(500)
+        widgetlog.setFixedWidth(400)
+        widgetlog.show()
+        
+        #logui.show()
+        sys.exit(app.exec_())
