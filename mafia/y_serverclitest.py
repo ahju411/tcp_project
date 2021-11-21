bc1d4aca3826575893c543fde975a0478aae5af2
Yoo <isz06188@naver.com>
2021-11-21 20:03:00 +0900

    sdfsdf

--- mafia/y_serverclitest.py
+++ mafia/y_serverclitest.py
@@ -36,6 +36,14 @@ class Client(object):
         self.chat_ui.setupUi(self.chatWidget)
         self.chat_ui.inputbutton.clicked.connect(self.send_message)
         self.chat_ui.textEdit.returnPressed.connect(self.send_message)
+        self.chat_ui.to_user1.clicked.connect(self.send_to_user1)
+        self.chat_ui.to_user2.clicked.connect(self.send_to_user2)
+        self.chat_ui.to_user3.clicked.connect(self.send_to_user3)
+        self.chat_ui.to_user4.clicked.connect(self.send_to_user4)
+        self.chat_ui.to_user5.clicked.connect(self.send_to_user5)
+        self.chat_ui.to_user6.clicked.connect(self.send_to_user6)
+        self.chat_ui.to_user7.clicked.connect(self.send_to_user7)
+        self.chat_ui.to_user8.clicked.connect(self.send_to_user8)
         self.mainWindow.setGeometry(QtCore.QRect(500, 20,754, 595))
         
 
@@ -85,14 +93,14 @@ class Client(object):
                 self.chat_ui.myjob_image.setText("마피아(Test)")
                 message = message[0:-2]
                 self.chat_ui.textBrowser.append(message)
-                self.chat_ui.textBrowser.append("죽일 사람을 선택하세요")
+                
             
         elif message[-2]=="@": # 시민팀은 [-2] == @
             if message[-1]=="!": # 경찰은 [-1] == !
                 self.chat_ui.myjob_image.setText("경찰(Test)")
                 message = message[0:-2]
                 self.chat_ui.textBrowser.append(message)
-                self.chat_ui.textBrowser.append("정체를 알고싶은사람을 선택하세요")
+                
         
         elif message[-2]=="#": # 타이머,시간은 [-2]== #
             if message[-1]=="!": # 타이머는 [-1]== ! 
@@ -102,8 +110,11 @@ class Client(object):
             elif message[-1]=="@": # Date은 [-1] == @
                     message = message[:-2]
                     self.chat_ui.Date.setText(message)
-        
 
+        elif message[-2]=="%": # 이거 유저버튼클릭하면 서버로 유저버튼 넘어가는데 되돌아오는거 방지하기위해 막음
+                pass
+
+        
         else: # 일반 채팅일경우 textbroswer로 모두에게 보여줌
             self.chat_ui.textBrowser.append(message)
 
@@ -126,6 +137,119 @@ class Client(object):
             
             return False
         
+
+    def send_to_user1(self):
+        message = self.chat_ui.to_user1.text()
+        message = message+"를 지목했습니다."+"%!"
+        try:
+            self.tcp_client.send(message.encode())
+        except Exception as e:
+            error = "Unable to send message '{}'".format(str(e))
+            print("[INFO]", error)
+            self.show_error("Server Error", error)
+        message = message[0:-2]
+        self.chat_ui.textBrowser.append(message)
+        
+    def send_to_user2(self):
+        message = self.chat_ui.to_user2.text()
+        message = message+"를 지목했습니다."+"%@"
+        try:
+            self.tcp_client.send(message.encode())
+        except Exception as e:
+            error = "Unable to send message '{}'".format(str(e))
+            print("[INFO]", error)
+            self.show_error("Server Error", error)
+        message = message[0:-2]    
+        self.chat_ui.textBrowser.append(message)
+    def send_to_user3(self):
+        message = self.chat_ui.to_user3.text()
+        message = message+"를 지목했습니다."+"%#"
+        try:
+            self.tcp_client.send(message.encode())
+        except Exception as e:
+            error = "Unable to send message '{}'".format(str(e))
+            print("[INFO]", error)
+            self.show_error("Server Error", error)
+        message = message[0:-2]    
+        self.chat_ui.textBrowser.append(message)
+
+    def send_to_user4(self):
+        message = self.chat_ui.to_user4.text()
+        message = message+"를 지목했습니다."+"%$"
+        try:
+            self.tcp_client.send(message.encode())
+        except Exception as e:
+            error = "Unable to send message '{}'".format(str(e))
+            print("[INFO]", error)
+            self.show_error("Server Error", error)
+        message = message[0:-2]    
+        self.chat_ui.textBrowser.append(message)
+
+    def send_to_user5(self):
+        message = self.chat_ui.to_user5.text()
+        message = message+"를 지목했습니다."+"%^"
+        try:
+            self.tcp_client.send(message.encode())
+        except Exception as e:
+            error = "Unable to send message '{}'".format(str(e))
+            print("[INFO]", error)
+            self.show_error("Server Error", error)
+        message = message[0:-2]    
+        self.chat_ui.textBrowser.append(message)
+
+    def send_to_user6(self):
+        message = self.chat_ui.to_user6.text()
+        message = message+"를 지목했습니다."+"%&"
+        try:
+            self.tcp_client.send(message.encode())
+        except Exception as e:
+            error = "Unable to send message '{}'".format(str(e))
+            print("[INFO]", error)
+            self.show_error("Server Error", error)
+        message = message[0:-2]
+        self.chat_ui.textBrowser.append(message)
+
+    def send_to_user7(self):
+        message = self.chat_ui.to_user7.text()
+        message = message+"를 지목했습니다."+"%*"
+        try:
+            self.tcp_client.send(message.encode())
+        except Exception as e:
+            error = "Unable to send message '{}'".format(str(e))
+            print("[INFO]", error)
+            self.show_error("Server Error", error)
+        message = message[0:-2]    
+        self.chat_ui.textBrowser.append(message)
+
+    def send_to_user8(self):
+        message = self.chat_ui.to_user8.text()
+        message = message+"를 지목했습니다."+"%("
+        try:
+            self.tcp_client.send(message.encode())
+        except Exception as e:
+            error = "Unable to send message '{}'".format(str(e))
+            print("[INFO]", error)
+            self.show_error("Server Error", error)
+        message = message[0:-2]    
+        self.chat_ui.textBrowser.append(message)
+                                        
+
+        
+
+
+    def send_message(self):
+        message = self.chat_ui.textEdit.text()
+        self.chat_ui.textBrowser.append("Me: " + message)
+
+        print("sent: " + message)
+
+        try:
+            self.tcp_client.send(message.encode())
+        except Exception as e:
+            error = "Unable to send message '{}'".format(str(e))
+            print("[INFO]", error)
+            self.show_error("Server Error", error)
+        self.chat_ui.textEdit.clear()
 
     def send_message(self):
         message = self.chat_ui.textEdit.text()
